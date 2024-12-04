9be45882414cfc0ba54e5047c1f752d736b06127
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2024-12-04 08:38:07 -0500

    Initial commit

--- app/page.js
+++ app/page.js
@@ -1,101 +1,70 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className='flex flex-col items-center mt-10 mb-20'>
+      <h1 className='text-5xl font-bold'>Asphalt Contractor Web Banners</h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+      <h2 className='text-3xl mt-10'>January</h2>
+
+      <section className='my-10'>
+        <h4 className='text-xl font-bold mb-5'>300 x 600</h4>
+        <a href='http://www.transtechsys.com/products/pqi380' target='_blank'>
+          <img src='/AC600x300Static.png' alt='600x300' className='w-[300px]' />
+        </a>
+        <h5 className='mt-3'>
+          <a href='/AC600x300StaticEx.png' target='_blank'>
+            Example Placement
           </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
+        </h5>
+      </section>
+
+      <section className='my-10'>
+        <h4 className='text-xl font-bold mb-5'>300 x 250</h4>
+        <a href='http://www.transtechsys.com/products/pqi380' target='_blank'>
+          <img
+            src='/AC300x250Animated.gif'
+            alt='300x250'
+            className='w-[300px]'
+          />
+        </a>
+        <h5 className='mt-3'>
+          <a href='/AC300x250AnimatedEx.png' target='_blank'>
+            Example Placement
           </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
+        </h5>
+      </section>
+
+      <section className='my-10'>
+        <h4 className='text-xl font-bold mb-5'>970 x 90</h4>
+        <a href='http://www.transtechsys.com/products/pqi380' target='_blank'>
+          <img src='/AC970x90Animated.gif' alt='970x90' className='w-[970px]' />
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
+        <h5 className='mt-3'>
+          <a href='/AC970x90AnimatedEx.png' target='_blank'>
+            Example Placement
+          </a>
+        </h5>
+      </section>
+
+      <section className='my-10'>
+        <h4 className='text-xl font-bold mb-5'>728 x 90</h4>
+        <a href='http://www.transtechsys.com/products/pqi380' target='_blank'>
+          <img src='/AC728x90Animated.gif' alt='728x90' className='w-[728px]' />
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+        <h5 className='mt-3'>
+          <a href='/AC728x90AnimatedEx.png' target='_blank'>
+            Example Placement
+          </a>
+        </h5>
+      </section>
+
+      <section className='my-10'>
+        <h4 className='text-xl font-bold mb-5'>300 x 50</h4>
+        <a href='http://www.transtechsys.com/products/pqi380' target='_blank'>
+          <img src='/AC300x50Static.png' alt='300X50' className='w-[300px]' />
         </a>
-      </footer>
+      </section>
     </div>
   );
 }
