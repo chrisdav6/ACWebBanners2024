cf5042071ae37570142768eeabdcd3678a5e23ac
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2024-12-05 09:10:13 -0500

    Added all banner sizes

--- app/page.js
+++ app/page.js
@@ -1,12 +1,10 @@
 export default function Home() {
   return (
     <div className='flex flex-col items-center mt-10 mb-20'>
-      <h1 className='text-5xl font-bold'>Asphalt Contractor Web Banners</h1>
+      <h2 className='text-5xl font-bold'>Asphalt Contractor Web Banner</h2>
 
-      <h2 className='text-3xl mt-10'>January</h2>
-
-      <section className='my-10'>
-        <h4 className='text-xl font-bold mb-5'>970 x 90</h4>
+      <section className='my-20'>
+        <h4 className='text-xl font-bold mb-5'>970 x 90 - Animated</h4>
         <a
           href='http://www.transtechsys.com/products/digitalAC'
           target='_blank'
@@ -20,35 +18,58 @@ export default function Home() {
         </h5>
       </section>
 
-      <section className='my-10'>
-        <h4 className='text-xl font-bold mb-5'>728 x 90</h4>
-        <a href='http://www.transtechsys.com/products/pqi380' target='_blank'>
-          <img src='/AC728x90Animated.gif' alt='728x90' className='w-[728px]' />
-        </a>
-        <h5 className='mt-3'>
-          <a href='/AC728x90AnimatedEx.png' target='_blank'>
-            Example Placement
-          </a>
-        </h5>
-      </section>
+      <h2 className='text-5xl font-bold'>Asphalt Pro Web Banner</h2>
 
-      <section className='my-10'>
-        <h4 className='text-xl font-bold mb-5'>300 x 600</h4>
-        <a href='http://www.transtechsys.com/products/pqi380' target='_blank'>
-          <img src='/AC600x300Static.png' alt='600x300' className='w-[300px]' />
-        </a>
-        <h5 className='mt-3'>
-          <a href='/AC600x300StaticEx.png' target='_blank'>
-            Example Placement
-          </a>
-        </h5>
-      </section>
-
-      <section className='my-10'>
-        <h4 className='text-xl font-bold mb-5'>300 x 250</h4>
-        <a href='http://www.transtechsys.com/products/pqi380' target='_blank'>
+      <section className='my-20'>
+        <h4 className='text-xl font-bold mb-5'>300 x 300 - Animated</h4>
+        <a
+          href='http://www.transtechsys.com/products/digitalAP'
+          target='_blank'
+        >
           <img
-            src='/AC300x250Animated.gif'
+            src='/AP300x300Animated.gif'
+            alt='300x300'
+            className='w-[300px]'
+          />
+        </a>
+        <h5 className='mt-3'>
+          <a href='/AC970x90AnimatedEx.png' target='_blank'>
+            Example Placement
+          </a>
+        </h5>
+      </section>
+
+      <h2 className='text-5xl font-bold'>Asphalt Pro Email ToolTip</h2>
+
+      <section className='my-20'>
+        <h4 className='text-xl font-bold mb-5'>640 x 220 - Static</h4>
+        <a
+          href='http://www.transtechsys.com/products/digitalAP'
+          target='_blank'
+        >
+          <img
+            src='/asphaltProWebBanner640x220.png'
+            alt='640x220'
+            className='w-[640px]'
+          />
+        </a>
+        <h5 className='mt-3'>
+          <a href='/AC970x90AnimatedEx.png' target='_blank'>
+            Example Placement
+          </a>
+        </h5>
+      </section>
+
+      <h2 className='text-5xl font-bold'>NAPA Web Banner</h2>
+
+      <section className='my-20'>
+        <h4 className='text-xl font-bold mb-5'>300 x 250 - Animated</h4>
+        <a
+          href='http://www.transtechsys.com/products/digitalNAPA'
+          target='_blank'
+        >
+          <img
+            src='/NAPA300x250Animated.gif'
             alt='300x250'
             className='w-[300px]'
           />
@@ -60,12 +81,57 @@ export default function Home() {
         </h5>
       </section>
 
-      <section className='my-10'>
+      <h2 className='text-5xl font-bold'>TXAPA Web Banner</h2>
+
+      <section className='my-20'>
+        <h4 className='text-xl font-bold mb-5'>300 x 250 - Animated</h4>
+        <a
+          href='http://www.transtechsys.com/products/digitalTXAPA'
+          target='_blank'
+        >
+          <img
+            src='/TXAPA300x250Animated.gif'
+            alt='300x250'
+            className='w-[300px]'
+          />
+        </a>
+        <h5 className='mt-3'>
+          <a href='/AC300x250AnimatedEx.png' target='_blank'>
+            Example Placement
+          </a>
+        </h5>
+      </section>
+
+      {/* <section className='my-10'>
+        <h4 className='text-xl font-bold mb-5'>728 x 90</h4>
+        <a href='http://www.transtechsys.com/products/pqi380' target='_blank'>
+          <img src='/AC728x90Animated.gif' alt='728x90' className='w-[728px]' />
+        </a>
+        <h5 className='mt-3'>
+          <a href='/AC728x90AnimatedEx.png' target='_blank'>
+            Example Placement
+          </a>
+        </h5>
+      </section> */}
+
+      {/* <section className='my-10'>
+        <h4 className='text-xl font-bold mb-5'>300 x 600</h4>
+        <a href='http://www.transtechsys.com/products/pqi380' target='_blank'>
+          <img src='/AC600x300Static.png' alt='600x300' className='w-[300px]' />
+        </a>
+        <h5 className='mt-3'>
+          <a href='/AC600x300StaticEx.png' target='_blank'>
+            Example Placement
+          </a>
+        </h5>
+      </section> */}
+
+      {/* <section className='my-10'>
         <h4 className='text-xl font-bold mb-5'>300 x 50</h4>
         <a href='http://www.transtechsys.com/products/pqi380' target='_blank'>
           <img src='/AC300x50Static.png' alt='300X50' className='w-[300px]' />
         </a>
-      </section>
+      </section> */}
     </div>
   );
 }
